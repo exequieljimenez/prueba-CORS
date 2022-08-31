5c1d07c5c85f142be6478f20d42423ae77ca2596
exequieljimenez <exequieljimenez@gmail.com>
2022-08-31 16:12:04 -0300

    segunda

--- js/clase-15-4.js
+++ js/clase-15-4.js
@@ -1,3 +1,8 @@
+let listaAlbumes
+
 fetch('albums.json')
     .then((respuesta) => respuesta.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+        console.log(data)
+        listaAlbumes = data
+    })
